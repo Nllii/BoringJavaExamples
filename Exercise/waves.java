efb373e9925124687e7a3b9c1d59d432874fc703
Nllii <nllii@pm.me>
2023-08-20 18:40:32 -0500

    added scripts

--- Exercise/waves.java
+++ Exercise/waves.java
@@ -4,26 +4,57 @@ package Exercise;
 
 
 public class waves {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         int stars = 0;
         for (stars = 0; stars < 10; stars ++){
-            // print stars -1 times for each iteration
+            // sleep for 1 second
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            // print stars +1 times for each iteration
             for (int i = 0; i < stars; i++){
                 System.out.print("*");
             }
             System.out.println();
-        }
-        for (stars = 10; stars > 0; stars --){
-            // print stars -1 times for each iteration
-            for (int i = 0; i < stars; i++){
-                System.out.print("*");
+            if (stars == 9){
+                for (stars = 10; stars > 0; stars --){
+                    // print stars -1 times for each iteration
+                    for (int i = 0; i < stars; i++){
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
             }
-            System.out.println();
+            // sleep for 1 second
         }
-        
-    }
 
-    
+
+    }
     
 }
+
+// try {
+    
+//         {
+//             int stars = 0;
+//             for (stars = 0; stars < 10; stars ++){
+//                 // print stars +1 times for each iteration
+//                 for (int i = 0; i < stars; i++){
+//                     System.out.print("*");
+//                 }
+//                 System.out.println();
+//             }
+//             for (stars = 10; stars > 0; stars --){
+//                 // print stars -1 times for each iteration
+//                 for (int i = 0; i < stars; i++){
+//                     System.out.print("*");
+//                 }
+//                 System.out.println();
+//             }
+            
+//         }
+// } catch (Exception e) {
+//     // TODO: handle exception
+// }
